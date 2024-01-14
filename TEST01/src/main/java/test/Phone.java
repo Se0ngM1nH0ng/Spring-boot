495c551ceff392b1da914ef35dafc0a1228be863
SeongMinHong <137602339+Se0ngM1nH0ng@users.noreply.github.com>
2024-01-14 23:02:29 +0900

    Delete TEST01 directory

--- TEST01/src/main/java/test/Phone.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public interface Phone {
-	void call(String name);
-	// 인터페이스에 달리는 속성 두가지 
-	// public : 부모의 공개범위가 >= 자식의 공개범위 보다 무조건 넓어야 하기 때문에 
-	// abstract :  추상이라서 메서드 바디를 가질 수 가 없다 
-}
