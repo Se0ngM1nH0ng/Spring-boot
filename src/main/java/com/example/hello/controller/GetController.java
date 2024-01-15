fc000ed8a0203f9ccba0706370b005787845dd7a
seongminHong1 <137602339+seongminHong1@users.noreply.github.com>
2024-01-15 23:17:08 +0900

    feat: dto, GetController

--- src/main/java/com/example/hello/controller/GetController.java
+++ src/main/java/com/example/hello/controller/GetController.java
@@ -1,25 +1,34 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
-    @GetMapping(value = "/variable1/{variable}")
+    @GetMapping(value = "/variable1/{variable}") // url에 바로 값을 넣음
     public String getVariable1(@PathVariable String variable) {
         return variable;
     }
-    @GetMapping(value = "/variable2/{variable}")
+    @GetMapping(value = "/variable2/{variable}") // 매개변수명 안맞을때 이어주는 것
     public String getVariable2(@PathVariable("variable") String var) {
         return var;
     }
 
-    @GetMapping
-    public String test(){
-
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String name , @RequestParam String email, @RequestParam String organization ){
+        return name + " " + email + " " + organization;
     }
+//    @GetMapping(value = "/request2")
+//    public String getRequestParam2(@RequestParam Map<String, String> map){
+//        StringBuilder sb = new StringBuilder();
+//
+//        param.entrySet().forEach(map -> {
+//           sb.append(map.getKey() + ":" + map.getValue() + "\n");
+//        });
+//
+//        return sb.toString();
+//    }
 }
---
+++ src/main/java/com/example/hello/dto/MemberDto.java
@@ -0,0 +1,11 @@
+package com.example.hello.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+
+    private String name;
+    private String email;
+    private String organization;
+}
